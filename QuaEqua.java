ccdbaecf2b678872340db88ea7e6f6f9ab28a126
margozamanova <106380217+margozamanova@users.noreply.github.com>
2022-07-29 16:38:52 +0300

    Update QuaEqua.java

--- QuaEqua.java
+++ QuaEqua.java
@@ -1,5 +1,3 @@
-package QE_TUSUR;
-
 public class QuaEqua {
     double a, b, c;
     double D;
@@ -20,20 +18,19 @@ public class QuaEqua {
             System.out.println("Это не квадратное уравнение! Вычисленный дискриминант не нужен!");
             double x;
             x = -c / b;
-            System.out.println("Неизвестная х равна " + x + ", а уравнение имеет вид "+ b + "x" + " + " + c + " = 0");
+            System.out.println("Неизвестная х равна " + String.format("%.2f", x) + ", а уравнение имеет вид "+ b + "x" + " + " + c + " = 0");
         }
-
         else if (D > 0) {
             double x1, x2;
             System.out.println("Вычислим корни уравнения х1 и х2 по формуле х1,2 = (-b +- sqr(D)) / 2a");
             x1 = (-b - Math.sqrt(D)) / (2 * a);
             x2 = (-b + Math.sqrt(D)) / (2 * a);
-            System.out.println("Корни уравнения: x1 = (-b - sqr(D)) / 2a = " + x1 + ", x2 = (-b + sqr(D)) / 2a = " + x2 + " при a = " + a + ", b = " + b + ", c = " + c + ".");
+            System.out.println("Корни уравнения: x1 = (-b - sqr(D)) / 2a = " + String.format("%.2f", x1) + ", x2 = (-b + sqr(D)) / 2a = " + String.format("%.2f", x2) + " при a = " + a + ", b = " + b + ", c = " + c + ".");
             System.out.println();
         } else if (D == 0) {
             double x;
             x = -b / (2 * a);
-            System.out.println("Уравнение имеет единственный корень: x = -b / (2a) = " + x + " при a = " + a + ", b = " + b + ", c = " + c + ".");
+            System.out.println("Уравнение имеет единственный корень: x = -b / (2a) = " + String.format("%.2f", x) + " при a = " + a + ", b = " + b + ", c = " + c + ".");
             System.out.println();
         } else
             System.out.println("Уравнение не имеет действительных корней при a = " + a + ", b = " + b + ", c = " + c + ".");
@@ -41,7 +38,7 @@ public class QuaEqua {
         }
     class QuaEquaTest1 {
         public static void main(String[] args) {
-            QuaEqua QE1 = new QuaEqua(1, 5, 7);
+            QuaEqua QE1 = new QuaEqua(4, 10, 3);
             QE1.Discriminant();
             QuaEqua QE2 = new QuaEqua(4, 4, 1);
             QE2.Discriminant();
@@ -51,4 +48,3 @@ public class QuaEqua {
             QE4.Discriminant();
         }
     }
-
